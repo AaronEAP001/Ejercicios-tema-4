abc19e0c6df6035962865595fae54e01c4729f71
AARON AQUINO <a20200705@idat.edu.pe>
2023-03-29 13:55:10 -0500

    desarrollo del ejercicio 3, uso del DO-While

--- src/ejercicio/main.java
+++ src/ejercicio/main.java
@@ -29,6 +29,13 @@ public class main {
             numeroWhile+=1;
         }
         
+        //ejercicio3
+        System.out.println("----- 3 -----");
+        do {            
+            System.out.println(String.format("Incrementa el Numero -> %s ", numeroWhile));
+            numeroWhile+=1;
+        } while (numeroWhile<3);
+        
         
     }
     
@@ -36,15 +43,10 @@ public class main {
 
 /*
 
-Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3, 
-el bloque de código que tendrá el bucle deberá:
- -> Incrementar el valor de la variable en uno cada vez que se ejecute.
- -> Mostrarlo por pantalla cada vez que se ejecute.
-
-Para el bucle Do While, deberás crear la misma estructura que en el While, pero solo se debe ejecutar una vez.
 Para el bucle For, crea una variable numeroFor, esta variable tendrá como valor 0 y 
 su condición será que la variable sea igual o menor que 3, se irá incrementando en 1 su valor cada vez que se ejecute y 
 deberá mostrarse por pantalla.
+
 Por último, para el Switch, deberás crear la variable estacion, y distintos case para las cuatro estaciones del año. 
 Dependiendo del valor de la variable estacion se deberá mandar un mensaje 
 por consola informando de la estación en la que está. 
